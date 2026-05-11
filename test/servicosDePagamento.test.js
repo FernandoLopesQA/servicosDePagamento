bab9849157abe7c842702fdce3896d3fabd5f022
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-05-10 22:20:33 -0300

    Implementado mapeamento de cenários a serem testados

--- test/servicosDePagamento.test.js
+++ test/servicosDePagamento.test.js
@@ -1,19 +1,57 @@
 import ServicosDePagamento from "../src/servicosDePagamento.js";
 import assert from 'node:assert'
 
-describe('Testes na classe de Serviço de Pagamento', () => {
+describe('Testes na classe de Serviços de Pagamento', () => {
 
-    it('', () => {
+    describe('Testes no método efetuarPagamento', () => {
 
-        // Arrange
+        // Implementar na função
+        // it('Deve retornar mensagem de erro quando algum dos dados não for passado nos parâmetros da função', () => {
+        //     // Arrange
+        //     // Act
+        //     // Assert
+        // });
 
+        it('Deve salvar todos os dados passados por parâmetro nas propriedades do pagamento', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
 
-        // Act
+        it('Deve efetuar inclusão da categoria "cara" quando valor de pagamento for maior que 100.00', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
 
+        it('Deve efetuar inclusão da categoria "padrão" quando valor de pagamento for menor que 100.00', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
 
-        // Assert
+        it('Deve efetuar inclusão da categoria "padrão" quando valor de pagamento for igual a 100.00', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
 
-        
-    })
+    });
 
-})
+    describe('Testes no método consultarUltimoPagamento', () => {
+
+        it('Deve retornar os dados do último pagamento registrado na lista', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        // Implementar na função
+        // it('Deve retornar mensagem de erro quando não houver dados de pagamento na lista', () => {
+        //     // Arrange
+        //     // Act
+        //     // Assert
+        // });
+
+    });
+});
