207cbc553cefb157577683077f54f9770b2c3a29
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-05-10 23:01:25 -0300

    Inclusão dos testes iniciais nos metodos da classe

--- src/servicosDePagamento.js
+++ src/servicosDePagamento.js
@@ -28,3 +28,11 @@ export default class ServicosDePagamento {
         return this.#pagamentos.at(-1);
     };
 };
+
+// Bloco de testes da função
+// const servico = new ServicosDePagamento();
+// servico.efetuarPagamento('1234-5678-1', 'Empresa Teste 1', 100.01);
+// servico.efetuarPagamento('1234-5678-2', 'Empresa Teste 2', 100.00);
+// servico.efetuarPagamento('1234-5678-3', 'Empresa Teste 3', 99);
+// servico.efetuarPagamento('1234-5678-4', 'Empresa Teste 4', 101);
+// console.log(servico.consultarUltimoPagamento());
--- test/servicosDePagamento.test.js
+++ test/servicosDePagamento.test.js
@@ -14,26 +14,50 @@ describe('Testes na classe de Serviços de Pagamento', () => {
 
         it('Deve salvar todos os dados passados por parâmetro nas propriedades do pagamento', () => {
             // Arrange
+            const servicosDePagamento = new ServicosDePagamento();
+
             // Act
+            servicosDePagamento.efetuarPagamento('7-891221-927787', 'Sul Gás', 147.29);
+            const pagamentos = servicosDePagamento.consultarUltimoPagamento();
+
             // Assert
+            assert.equal(pagamentos.codigoBarras, '7-891221-927787');
+            assert.equal(pagamentos.empresa, 'Sul Gás');
+            assert.equal(pagamentos.valor, 147.29);
+            assert.equal(pagamentos.categoria, 'cara');
         });
 
         it('Deve efetuar inclusão da categoria "cara" quando valor de pagamento for maior que 100.00', () => {
-            // Arrange
+            const servicosDePagamento = new ServicosDePagamento();
+
             // Act
+            servicosDePagamento.efetuarPagamento('7-894404-377140', 'Claro Móvel', 100.01);
+            const pagamentos = servicosDePagamento.consultarUltimoPagamento();
+
             // Assert
+            assert.equal(pagamentos.categoria, 'cara');
         });
 
         it('Deve efetuar inclusão da categoria "padrão" quando valor de pagamento for menor que 100.00', () => {
-            // Arrange
+            const servicosDePagamento = new ServicosDePagamento();
+
             // Act
+            servicosDePagamento.efetuarPagamento('7-890765-501033', 'NetFlix', 99.99);
+            const pagamentos = servicosDePagamento.consultarUltimoPagamento();
+
             // Assert
+            assert.equal(pagamentos.categoria, 'padrão');
         });
 
         it('Deve efetuar inclusão da categoria "padrão" quando valor de pagamento for igual a 100.00', () => {
-            // Arrange
+            const servicosDePagamento = new ServicosDePagamento();
+
             // Act
+            servicosDePagamento.efetuarPagamento('7-890274-041686', 'Consórcio', 100.00);
+            const pagamentos = servicosDePagamento.consultarUltimoPagamento();
+
             // Assert
+            assert.equal(pagamentos.categoria, 'padrão');
         });
 
     });
@@ -41,9 +65,21 @@ describe('Testes na classe de Serviços de Pagamento', () => {
     describe('Testes no método consultarUltimoPagamento', () => {
 
         it('Deve retornar os dados do último pagamento registrado na lista', () => {
-            // Arrange
+            const servicosDePagamento = new ServicosDePagamento();
+
             // Act
+            servicosDePagamento.efetuarPagamento('7-891221-927787', 'Sul Gás', 147.29);
+            servicosDePagamento.efetuarPagamento('7-894404-377140', 'Claro Móvel', 100.01);
+            servicosDePagamento.efetuarPagamento('7-890765-501033', 'NetFlix', 99.99);
+            servicosDePagamento.efetuarPagamento('7-890274-041686', 'Consórcio', 100.00);
+            servicosDePagamento.efetuarPagamento('7-894415-027614', 'Pós Júlio de Lima', 447.47);
+            const pagamentos = servicosDePagamento.consultarUltimoPagamento();
+
             // Assert
+            assert.equal(pagamentos.codigoBarras, '7-894415-027614');
+            assert.equal(pagamentos.empresa, 'Pós Júlio de Lima');
+            assert.equal(pagamentos.valor, 447.47);
+            assert.equal(pagamentos.categoria, 'cara');
         });
 
         // Implementar na função
